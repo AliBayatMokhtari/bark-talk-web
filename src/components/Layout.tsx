8673102c7d8eb7f2f831790db9194d5df5b4921f
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2023-09-12 18:50:33 +0330

    [feat][module] web services

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,40 @@
-import Message from "./chat/Message";
+import { useState } from "react";
+import Message from "./chat/message";
 import { Button } from "./ui/button";
 import { Card } from "./ui/card";
+import audioRecorder from "@/lib/audioRecorder";
+import converter from "@/lib/converter";
+import webService from "@/service/webService";
 
 export default function Layout() {
+  const [isRecording, setIsRecording] = useState<boolean>(false);
+
+  const startRecording = () => {
+    setIsRecording(true);
+    audioRecorder.start().then(() => {
+      console.log("Recording audio");
+    });
+  };
+
+  const stopRecording = () => {
+    setIsRecording(false);
+    audioRecorder
+      .stop()
+      .then((blob) => converter.blobToBase64(blob))
+      .then((res) => {
+        const reqBody = {
+          file_name:
+            Math.floor(Math.random() * (99999999999 - 1 + 1) + 1) + ".wav",
+          file_content: res,
+        };
+        webService.upload("en", reqBody).then((res) => console.log(res));
+      });
+  };
+
+  const getLangs = () => {
+    webService.getSupportedLangs().then((res) => console.log(res));
+  };
+
   return (
     <div className="w-full h-full bg-slate-100">
       <div className="flex flex-col h-full max-w-3xl gap-2 p-8 mx-auto">
@@ -17,7 +49,9 @@ export default function Layout() {
           </div>
           <div className="flex items-center gap-2 p-2">
             <div className="border border-solid h-[35px] rounded-sm flex-1"></div>
-            <Button>Record</Button>
+            <Button onClick={isRecording ? stopRecording : startRecording}>
+              {isRecording ? "Stop Recording" : "Record Voice"}
+            </Button>
           </div>
         </Card>
       </div>
---
+++ src/service/webService.ts
@@ -0,0 +1,65 @@
+const baseUrl = "http://3.67.227.198:5002";
+
+interface ServerResultModel<T> {
+  data: T;
+  message: string | null;
+  status: "success" | "failed";
+}
+
+type SupportedLang = Record<string, string>;
+
+interface WebService {
+  getSupportedLangs(): Promise<ServerResultModel<SupportedLang>>;
+  upload(
+    lang: string,
+    payload: {
+      file_name: string;
+      file_content: string;
+    }
+  ): Promise<
+    ServerResultModel<{
+      answer: {
+        answer_language: string;
+        file_name: string;
+        text: string;
+        voice_path: string;
+      };
+      prompt: string;
+      prompt_language: string;
+    }>
+  >;
+  download(): void;
+}
+
+const webService: WebService = {
+  getSupportedLangs() {
+    const url = baseUrl + "/supported_language";
+    return new Promise((resolve) =>
+      fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => resolve(json))
+    );
+  },
+  async upload(lang, payload) {
+    const url = baseUrl + `/ask/web/${lang}`;
+    return new Promise((resolve) =>
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      })
+        .then((response) => response.json())
+        .then((json) => resolve(json))
+    );
+  },
+  download() {},
+};
+
+export default webService;
